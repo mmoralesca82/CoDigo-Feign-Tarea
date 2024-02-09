69fcf1f80c345f667630668d20330205ec05d186
manuel <mmoralesca82@gmail.com>
2024-02-09 17:01:41 -0500

    feat: cambiando url de api Sunat

--- src/main/java/com/codigo/feign/feignclient/SunatClient.java
+++ src/main/java/com/codigo/feign/feignclient/SunatClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "sunat-client", url = "https://api.apis.net.pe/v2/sunat/")
+@FeignClient(name = "sunat-client", url = "https://api.apis.net.pe/v1/")
 public interface SunatClient {
     @GetMapping("/ruc")
     SunatResponse getInfo(@RequestParam("numero") String numero,
